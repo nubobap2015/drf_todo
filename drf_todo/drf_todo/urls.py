fe57543f1779c1adcfd3ed169059cf64fd3a123b
Sergey Kopeykin <nubobap2015@yandex.ru>
2022-02-26 14:43:41 +0300

    Lesson 1

--- drf_todo/drf_todo/urls.py
+++ drf_todo/drf_todo/urls.py
@@ -15,8 +15,15 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+from my_users.views import CustomUserModelViewSet
+
+
+router = DefaultRouter()
+router.register('users', CustomUserModelViewSet)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api-auth/', include('rest_framework.urls')),
+    path('api/', include(router.urls))
 ]
